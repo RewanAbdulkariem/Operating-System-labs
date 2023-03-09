acf4c83f33b189f4d700df543455f5f5d8be8294
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-09 16:29:23 +0200

    excutable lab1

--- lab-1/main.h
+++ lab-1/main.h
@@ -2,7 +2,12 @@
 // Created by rewan on 3/7/23.
 //
 
-#ifndef LAB_1_MAIN_H
-#define LAB_1_MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#endif //LAB_1_MAIN_H
+void on_child_exit(int signal);
+void setup_environment();
+void shell();
+void execute_command(char **command, int arg_length);
+
+#endif //MAIN_H
