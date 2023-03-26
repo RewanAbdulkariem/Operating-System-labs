1510f2dbcc5dba57ad05d48bf5c7cfba24bb00ba
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-26 03:40:56 +0200

    add readme

--- lab-3/caltrain.h
+++ lab-3/caltrain.h
@@ -4,5 +4,17 @@
 
 #ifndef LAB_3_CALTRAIN_H
 #define LAB_3_CALTRAIN_H
+#include <pthread.h>
 
+struct station {
+    // FILL ME IN
+};
+
+void station_init(struct station *station);
+
+void station_load_train(struct station *station, int count);
+
+void station_wait_for_train(struct station *station);
+
+void station_on_board(struct station *station);
 #endif //LAB_3_CALTRAIN_H
